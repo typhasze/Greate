4f3ba2b4e2bfe8db7de8353523262ee9661abecc
typhasze <48575093+typhasze@users.noreply.github.com>
2020-02-12 21:57:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/greate/RecipeSelection.java
@@ -0,0 +1,14 @@
+package com.example.greate;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_selection);
+    }
+}
